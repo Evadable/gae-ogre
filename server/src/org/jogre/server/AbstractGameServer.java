8495629dcdbace22b9c545c4dc1ca6d7cb50019d
schefflerjens <schefflerjens@cbea629a-434c-11de-af2b-f15df6ab9c44>
2009-05-23 04:05:18 +0000

    Pulled non-socket logic out of JogreServer into a superclass (this part should be reusable in App Engine).

---
+++ server/src/org/jogre/server/AbstractGameServer.java
@@ -0,0 +1,164 @@
+/*
+ * JOGRE (Java Online Gaming Real-time Engine) - Server
+ * Copyright (C) 2004  Bob Marks (marksie531@yahoo.com)
+ * http://jogre.sourceforge.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.jogre.server;
+
+import org.jogre.common.GameList;
+import org.jogre.common.IJogre;
+import org.jogre.common.util.JogreLogger;
+import org.jogre.server.controllers.ServerControllerList;
+import org.jogre.server.data.IServerData;
+
+public abstract class AbstractGameServer {
+
+  // Singleton instance of this server
+  private static AbstractGameServer instance;
+  
+  /**
+   * Return the single instance of this server.
+   *
+   * @return
+   */
+  public static AbstractGameServer getInstance () {
+    return instance;
+  }
+  
+  /** Logging */
+  JogreLogger logger = new JogreLogger (this.getClass());
+  
+  /** Boolean to specify if this server tries to link to a master server. */
+  private boolean linkToMasterServer = false;
+
+  /** List of server connenction objects. */
+  private final ConnectionList connections;
+
+  /** Link to the various games, and tables within games. */
+  private GameList gameList;
+  
+  /** Link to the various parsers. */
+  private ServerControllerList serverControllerList;
+
+  /** Game loader. */
+  private GameLoader gameLoader;
+  
+  /** Declare how a user connection. */
+  private final IServerData dataConnection;
+      
+  ServerLabels labels = ServerLabels.getInstance();   // convience link to server labels
+    
+  /**
+   * Default server constructor.
+   */
+  AbstractGameServer (ConnectionList connections, IServerData dataConnection) {
+    
+    // Set the singleton instance
+    if (instance != null) {
+      throw new AssertionError("There should only be one JogreServer!");
+    }
+    instance = this;
+    
+    // Set final fields
+    this.connections = connections;
+    this.dataConnection = dataConnection;
+    
+    // Type initial games server info.
+    System.out.println ("------------------------------------------------------------------");
+    System.out.println ("                J O G R E   G A M E S   S E R V E R");
+    System.out.println ("------------------------------------------------------------------");
+    System.out.println (labels.get("author") + ":\t\t\tBob Marks");
+    System.out.println (labels.get("version") + ":\t\t" + IJogre.VERSION);
+
+  }
+
+  /**
+   * Initialise the JOGRE server.
+   */
+  public void init () {
+    // Setup fields
+    this.serverControllerList = new ServerControllerList ();
+    this.gameList             = new GameList ();
+    this.gameLoader           = new GameLoader (gameList, serverControllerList);
+  }
+
+  /**
+   * Return the current list of games on this server.
+   *
+   * @return
+   */
+  public GameList getGameList () {
+    return gameList;
+  }
+
+  /**
+   * Set the link to the master server.
+   *
+   * @param linkToMasterServer
+   */
+  public void setLinkToMasterServer (boolean linkToMasterServer) {
+    this.linkToMasterServer = linkToMasterServer;
+  }
+
+  /**
+   * Return true if this server wishes to try and link to the
+   * master JOGRE game server.
+   *
+   * @return
+   */
+  public boolean isLinkToMasterServer () {
+    return linkToMasterServer;
+  }
+
+  /**
+   * Return the ConnectionList object of connections to the server.
+   *
+   * @return
+   */
+  public ConnectionList getConnections () {
+    return connections;
+  }
+
+  /**
+   * Return the server parser list which includes the standard base, game
+   * and table parser.  Also includes parsers for each specific game.
+   *
+   * @return   List of server parsers
+   */
+  public ServerControllerList getControllers () {
+    return serverControllerList;
+  }
+
+  /**
+   * Return the connection to the users.
+   * 
+   * @return
+   */
+  public IServerData getServerData () {
+    return dataConnection;
+  }
+
+  /**
+   * Return the game loader.
+   * 
+   * @return
+   */
+  public GameLoader getGameLoader() {
+    return gameLoader;
+  }
+  
+}
