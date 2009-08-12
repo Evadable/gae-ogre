bcbffb345ec743632557c9c4374e955c045cce5d
schefflerjens <schefflerjens@cbea629a-434c-11de-af2b-f15df6ab9c44>
2009-08-12 13:52:00 +0000

    started collecting external jar files needed for this project

---
+++ appengine/jogre-appengine/update_jars.sh
@@ -0,0 +1,5 @@
+cp ../../api/lib/*.jar war/WEB-INF/lib/
+cp ../../server/lib/proto*.jar war/WEB-INF/lib/
+cp ../../server/lib/dom4j.jar war/WEB-INF/lib/
+cp ../../api/jogre.jar war/WEB-INF/lib/
+cp ../../server/server.jar war/WEB-INF/lib/jogre_server.jar
